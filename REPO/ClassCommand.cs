dcefe9847cccb57d06996c22c4373cbf53461871
Donzulo <mads5376@edu.campusvejle.dk>
2019-04-09 13:03:19 +0200

    Created ClassCommand

---
+++ REPO/ClassCommand.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace REPO
+{
+    public class ClassCommand : ICommand
+    {
+        private Action action;
+        private bool canExecute;
+
+        public event EventHandler CanExecuteChanged;
+
+        public ClassCommand(Action inAction, bool inCanExecute)
+        {
+            action = inAction;
+            canExecute = inCanExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return canExecute;
+        }
+
+        public void Execute(object parameter)
+        {
+            action();
+        }
+    }
+}
